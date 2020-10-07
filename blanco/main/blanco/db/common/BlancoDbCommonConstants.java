5490b2c00787970cbe6296dae887109d964c89bf
tueda <ueo@ueo.co.jp>
2020-10-07 19:57:30 +0900

    [POI4] 2.2.0: Now ready to use apache poi 4.1.2.

--- blanco/main/blanco/db/common/BlancoDbCommonConstants.java
+++ blanco/main/blanco/db/common/BlancoDbCommonConstants.java
@@ -20,7 +20,7 @@ public class BlancoDbCommonConstants {
      * 項目番号:3<br>
      * バージョン番号。
      */
-    public static final String VERSION = "2.1.1.";
+    public static final String VERSION = "2.2.0";
 
     /**
      * 項目番号:4<br>
